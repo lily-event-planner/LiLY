b1a4a1186996eb3edcc0798b6086e1e510c431ac
hibtmimi <hibatamimi24@gmail.com>
2020-12-09 20:47:46 +0200

    controller

---
+++ back/routers/Route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+// const auth = require("../controller/auth")
+
+// router.post('/signupClient' ,signClient.signup);
+// router.post('/signinClient' ,signClient.signin);
+
+module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,41 +1,24 @@
-const { Router } = require("express");
-const express = require("express");
-const env = require("dotenv");
-const app = express();
-const bodyParser = require("body-parser");
-
-// routes
-const authRoute = require("./routs/auth");
-const adminRoute = require("./routs/admin/auth");
-
-//mongodb connect
 const mongoose = require("mongoose");
-const { connected } = require("process");
-const URI =
+const Route = require("./routers/Route");
+const URL =
   "mongodb+srv://hibatamimi:1141688Hhh.@cluster0.3obps.mongodb.net/LiLY?retryWrites=true&w=majority";
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
 
-//enviroment variable
-env.config();
+  .then(() => console.log("MongoDB Connected correctly ..."))
+  .catch((err) => console.log(err));
 
-//mongodb connect
-mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-const db = mongoose.connection;
-db.once("open", () => {
-  console.log("connected to db");
-});
+const express = require("express");
+const app = express();
 
-//midleware
-app.use(bodyParser());
-app.use("/", authRoute);
-app.use("/", adminRoute);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.use(express.urlencoded({ extended: true })) // to parse the data
-// to make the content header application/json
+// app.use("/", Route);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Listening on port ${process.env.PORT}`)
-);
+app.listen(8080, () => console.log(`Listening on port 8080`));
